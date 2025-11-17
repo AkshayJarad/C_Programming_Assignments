e135af6e5590e200fffee8dee9bd61231417bb90
Akshay Sanjay Jarad <157938695+AkshayJarad@users.noreply.github.com>
2025-11-17 11:36:34 +0530

    Add files via upload

---
+++ Assignments/Assignment_4/program4_5.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int FactDiff(int iNo)
+{
+    int iCnt = 0;
+    int iSumFact = 0;
+    int iSumNonFact = 0;
+    int iDef = 0;
+
+    for(iCnt = 1; iCnt <= (iNo/2); iCnt++)
+    {
+        if(iNo % iCnt == 0)
+        {
+            iSumFact = iSumFact + iCnt;
+        }
+    }
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        if(iNo % iCnt != 0)
+        {
+            iSumNonFact = iSumNonFact + iCnt;
+        }
+
+    }
+    iDef = iSumFact - iSumNonFact;
+    return iDef;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter the number : \n");
+    scanf("%d",&iValue);
+
+    iRet = FactDiff(iValue);
+
+    printf("%d\n",iRet);
+
+    return 0;
+}
