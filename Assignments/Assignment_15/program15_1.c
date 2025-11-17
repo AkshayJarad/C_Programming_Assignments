2857fb8201adb1276fe238dfa159a7d93d186f03
Akshay Sanjay Jarad <157938695+AkshayJarad@users.noreply.github.com>
2025-11-17 12:21:10 +0530

    Add files via upload

---
+++ Assignments/Assignment_15/program15_1.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int CountEven(int iNo)
+{
+    int iDigit = 0;
+    int freq = 0;
+
+    if(iNo < 0)
+    {
+        iNo = - iNo;
+    }
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        iNo = iNo / 10;
+        if(iDigit % 2 ==  0)
+        {
+            freq++;
+        }
+    }
+    return freq;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    iRet = CountEven(iValue);
+
+    printf("%d\n",iRet);
+    
+    return 0;
+}
