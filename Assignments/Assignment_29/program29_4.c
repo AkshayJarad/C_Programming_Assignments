e4e3fd99ce02540104fd360b95d860be3cdd9e2b
Akshay Sanjay Jarad <157938695+AkshayJarad@users.noreply.github.com>
2026-02-17 19:42:29 +0530

    Add files via upload

---
+++ Assignments/Assignment_29/program29_4.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0, inum1 = -1, inum2 = 1;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i % 2 == 0)
+            {
+                printf("%d\t",inum1);
+                inum1 = inum1 - 1;
+            }
+            else
+            {
+                printf("%d\t",inum2);
+                inum2 = inum2 + 1;
+            }
+
+            if(inum1 == -6 || inum2 == 6)
+            {
+                inum1 = -1;
+                inum2 = 1;
+            }
+        }
+        printf("\n\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the number of rows and columns : \n");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
